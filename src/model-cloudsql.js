537f0c9a9ae609d70fdd4e778cda4449f4886541
Aman Ibrahim <amanmibra@gmail.com>
2017-11-08 23:40:16 -0500

    Removed unnecessarry functions

--- src/model-cloudsql.js
+++ src/model-cloudsql.js
@@ -15,76 +15,8 @@ if (config['INSTANCE_CONNECTION_NAME'] && config['NODE_ENV'] === 'production') {
 
 const connection = mysql.createConnection(options);
 
-// [START list]
-function list (limit, token, cb) {
-  token = token ? parseInt(token, 10) : 0;
-  connection.query(
-    'SELECT * FROM `books` LIMIT ? OFFSET ?', [limit, token],
-    (err, results) => {
-      if (err) {
-        cb(err);
-        return;
-      }
-      const hasMore = results.length === limit ? token + results.length : false;
-      cb(null, results, hasMore);
-    }
-  );
-}
-// [END list]
-
-// [START create]
-function create (data, cb) {
-  connection.query('INSERT INTO `books` SET ?', data, (err, res) => {
-    if (err) {
-      cb(err);
-      return;
-    }
-    read(res.insertId, cb);
-  });
-}
-// [END create]
-
-function read (id, cb) {
-  connection.query(
-    'SELECT * FROM `books` WHERE `id` = ?', id, (err, results) => {
-      if (!err && !results.length) {
-        err = {
-          code: 404,
-          message: 'Not found'
-        };
-      }
-      if (err) {
-        cb(err);
-        return;
-      }
-      cb(null, results[0]);
-    });
-}
-
-// [START update]
-function update (id, data, cb) {
-  connection.query(
-    'UPDATE `books` SET ? WHERE `id` = ?', [data, id], (err) => {
-      if (err) {
-        cb(err);
-        return;
-      }
-      read(id, cb);
-    });
-}
-// [END update]
-
-function _delete (id, cb) {
-  connection.query('DELETE FROM `books` WHERE `id` = ?', id, cb);
-}
-
 module.exports = {
-  createSchema: createSchema,
-  list: list,
-  create: create,
-  read: read,
-  update: update,
-  delete: _delete
+  createSchema: createSchema
 };
 
 if (module === require.main) {
